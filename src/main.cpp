562ee67d6322361c1805f108a296cb759b0bfcc6
Marcin <slawicz@echostar.pl>
2019-07-06 15:51:00 +0200

    main modified; a lot of commented lines so far

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,47 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Ac6
+  * @author  Marcin
   * @version V1.0
   * @date    01-December-2013
   * @brief   Default main function.
+  * @project 3dYoke
   ******************************************************************************
 */
 
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
+//#include "system.h"
+//#include "gpio.h"
+//#include "timer.h"
 			
 
 int main(void)
 {
+    //System::getInstance().config();
 
-	for(;;);
+    //GPIO pushbutton(USER_BUTTON_GPIO_PORT, USER_BUTTON_PIN, GPIO_MODE_INPUT, GPIO_PULLUP);
+    //GPIO led(LED2_GPIO_PORT, LED2_PIN, GPIO_MODE_OUTPUT_PP);
+    //Timer ledTimer;
+
+    // start reception of the first character
+    //System::getInstance().getConsole()->getInterface().startReception();
+
+    // send first prompt
+    //System::getInstance().getConsole()->sendPrompt();
+
+    // main loop
+    while(1)
+    {
+//        if(ledTimer.elapsed(500000))
+//        {
+//            led.toggle();
+//            ledTimer.reset();
+//        }
+//
+//        System::getInstance().getConsole()->handler();
+    }
+
+    //System::getInstance().terminate();
 }
